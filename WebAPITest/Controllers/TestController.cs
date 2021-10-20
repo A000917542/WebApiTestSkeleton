317f429fafd1905e212041207c98be6633763f73
Brent Ritchie <ritchie.brent@gmail.com>
2021-10-20 17:37:36 -0400

    Add project files.

---
+++ WebAPITest/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        //[FromQuery]
+        //public int inputStandard { get; set; }
+
+        [HttpGet]
+        public int Get([FromQuery] int input)
+        {
+            return input + 1;
+        }
+
+        [HttpDelete]
+        public int Delete([FromQuery] char input, [FromQuery] char input2)
+        {
+            return input + input2;
+        }
+    }
+}
